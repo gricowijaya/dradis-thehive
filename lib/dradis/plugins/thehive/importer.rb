56910cef2ff588a4a2da319b959adf7d29203521
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-04-23 02:47:30 +0800

    Testing JSON Parser for checking TheHive data

--- lib/dradis/plugins/thehive/importer.rb
+++ lib/dradis/plugins/thehive/importer.rb
@@ -2,7 +2,7 @@ module Dradis::Plugins::TheHive
   class Importer < Dradis::Plugins::Upload::Importer
 
     def self.templates
-      { issue: 'warning' }
+      { issue: 'issue' }
     end
 
     # The framework will call this function if the user selects this plugin from
@@ -16,17 +16,23 @@ module Dradis::Plugins::TheHive
       logger.info { "Parsing TheHive output from #{ params[:file] }..." }
       data = MultiJson.decode(file_content)
       logger.info { 'Done.' }
-      logger.info { "This is the Data from The hive Case #{ data }"} # Added by Rico  
+      logger.info { "There are #{ data.length } Case from TheHive."} # Added by Rico  
 
-      unless data.key?("case_export")
-        logger.error "ERROR: no 'case_export' field present in the provided "\
-                     "data. Are you sure you uploaded a TheHive file?"
-        exit(-1)
+      if data[0].key?("_type") && data[0].key?("_createdBy") && data[0].key?("severityLabel") && data[0].key?("tlpLabel") && data[0].key?("papLabel")
+        logger.info { "This JSON file appears to be from TheHive." }
+      else
+        logger.info { "This JSON file does not appear to be from TheHive." }
       end
+      #unless data.key?("case_export")
+      #  logger.error "ERROR: no 'case_export' field present in the provided "\
+      #               "data. Are you sure you uploaded a TheHive file?"
+      #  #exit(-1)
+      #  return false
+      #end
 
       # choose a different parent based on the application path?
-      case_export = template_service.process_template(template: 'case_export', data: data['scan_info'])
-      content_service.create_note text: case_export
+      #case_export = template_service.process_template(template: 'case_export', data: data)
+      #content_service.create_note text: case_export
 
       #logger.info { "#{data['warnings'].count} Warnings\n===========" }
 
@@ -36,7 +42,10 @@ module Dradis::Plugins::TheHive
       #  warning_info = template_service.process_template(template: 'warning', data: warning)
       #  content_service.create_issue text: warning_info, id: warning['warning_code']
       #end
+      return true
 
     end
+
+    def process_case_item()
   end
 end
